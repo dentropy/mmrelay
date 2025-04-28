4b7321a716422945e372304df3565d52ef712fb4
Paul Mullins <paulmullins@protonmail.com>
2025-04-28 05:36:29 -0400

    Added in some queries

--- my-schema/database/queries.sql
+++ my-schema/database/queries.sql
@@ -1,14 +1,16 @@
-SELECT DISTINCT data->>'pubkey'
-FROM nostr_events
-WHERE data ? 'pubkey';
+select * from normalized_nostr_events_t;
+select count(*) from normalized_nostr_events_t;
+select count(distinct(pubkey)) from normalized_nostr_events_t ;
+SELECT pubkey, COUNT(*) AS pubkey_note_count
+FROM normalized_nostr_events_t
+GROUP BY pubkey
+order by pubkey_note_count desc
+limit 100;
 
-select distinct pubkey from normalized_nostr_events_t;
+select * from nostr_event_on_relay_t;
+select count(*) from nostr_event_on_relay_t;
 
-select count(distinct pubkey) from normalized_nostr_events_t;
+select * from scraping_nostr_filters_t;
 
-
-SELECT * FROM normalized_nostr_events_t
-WHERE (
-    id in ids,
-    
-);
+select * from nostr_filter_scraping_logs_t;
+select count(*) from nostr_filter_scraping_logs_t;

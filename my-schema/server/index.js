f962235e8fc5f7b547bd3ca6f9266771894135c9
Paul Mullins <paulmullins@protonmail.com>
2025-04-29 21:11:24 -0400

    Getting started on the relay implimentation

--- my-schema/server/index.js
+++ my-schema/server/index.js
@@ -1,24 +1,49 @@
-import WebSocket, { WebSocketServer } from 'ws';
+import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({
-  port: 8080,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      // See zlib defaults.
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    // Other options settable:
-    clientNoContextTakeover: true, // Defaults to negotiated value.
-    serverNoContextTakeover: true, // Defaults to negotiated value.
-    serverMaxWindowBits: 10, // Defaults to negotiated value.
-    // Below options specified as default values.
-    concurrencyLimit: 10, // Limits zlib concurrency for perf.
-    threshold: 1024 // Size (in bytes) below which messages
-    // should not be compressed if context takeover is disabled.
-  }
+const wss = new WebSocketServer({ port: 9090 });
+
+let subscriptions = {}
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    let json_parsed_data = {}
+    try {
+      json_parsed_data = JSON.stringify(data)
+    } catch (error) {
+      ws.send(JSON.stringify(["NOTICE", `Unable to process process JSON Data you provided,\n${data}\n\nCheck the Nostr Docs https://github.com/nostr-protocol/nips/blob/master/01.md`]));
+      return
+    }
+    try {
+      if (!(json_parsed_data[0] in ["EVENT", "REQ", "CLOSE"])){
+        ws.send(JSON.stringify(["NOTICE", `Unable to process process JSON Data you provided Please provide a list with the first item being "EVENT", "REQ"\n${data}\n\nCheck the Nostr Docs https://github.com/nostr-protocol/nips/blob/master/01.md`]));
+        return
+      }
+    } catch (error) {
+      ws.send(JSON.stringify(["NOTICE", `Unable to process process JSON Data you provided Please provide a list,\n${data}\n\nCheck the Nostr Docs https://github.com/nostr-protocol/nips/blob/master/01.md`]));
+    }
+    if (json_parsed_data[0] == "REQ") {
+
+    }
+    // Process the REQ filter
+      // If the Filter ID is already being used throw an Error
+      // Validate the Filter
+    // Perform SQL Query With Filter
+    // Return Results via ws.send
+    // Send CLOSE cause EOSE is not supported, maybe send a NOTICE
+
+    // TODO Accept new EVENTS
+    if (json_parsed_data[0] == "EVENT") {
+      // Accept EVENT
+      // Send OK Message Back
+      ws.send(JSON.stringify(["NOTICE", `EVENT message is not supported yet, You can't send events to this relay`]));
+    }
+
+
+    ws.send('something');
+  });
+
+  ws.send('Connected');
 });

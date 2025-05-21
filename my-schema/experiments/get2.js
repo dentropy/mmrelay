fb45b46ec0b0142536bc87c70819ef84885d5269
dentropy <dentropy@pop-os.localdomain>
2025-05-21 17:08:11 -0400

    Fix a bug

--- my-schema/experiments/get2.js
+++ my-schema/experiments/get2.js
@@ -2,16 +2,18 @@ import { v4 as uuidv4 } from 'uuid';
 import assert from "assert"
 import { generateSecretKey, getPublicKey, finalizeEvent, verifyEvent } from 'nostr-tools/pure'
 
-const wsPublish = new WebSocket("ws://localhost:9090")
-const wsListen = new WebSocket("ws://localhost:9090")
-let kind_stats = {}
+// const wsPublish = new WebSocket("ws://localhost:9090")
+// let kind_stats = {}
+
+const wsListen = new WebSocket("https://archive.mememaps.net/")
+// const wsListen = new WebSocket("http://localhost:9090")
 
 
 wsListen.addEventListener('open', async function (event) {
     wsListen.send(JSON.stringify([
         "REQ",
         String(uuidv4()),
-        JSON.stringify({ search: "bitcoin" }),
+        { search: "bitcoin" },
     ]));
 });
 wsListen.addEventListener('message', function (event) {

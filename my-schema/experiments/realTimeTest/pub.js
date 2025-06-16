cde0bf432f11041b73febb813ed9ff22c802fd56
Paul <paulmullins@protonmail.com>
2025-06-16 01:06:52 -0400

    Added basic scripts for testing

---
+++ my-schema/experiments/realTimeTest/pub.js
@@ -0,0 +1,26 @@
+import { generateSecretKey, getPublicKey } from 'nostr-tools/pure'
+
+let sk = generateSecretKey() // `sk` is a Uint8Array
+let pk = getPublicKey(sk) // `pk` is a hex string
+
+import { finalizeEvent, verifyEvent } from 'nostr-tools/pure'
+
+let event = finalizeEvent({
+  kind: 999,
+  created_at: Math.floor(Date.now() / 1000),
+  tags: [],
+  content: 'hello',
+}, sk)
+
+let isGood = verifyEvent(event)
+
+console.log(event)
+
+import { Relay } from "nostr-tools";
+const relay = new Relay("ws://localhost:9091")
+// const relay = new Relay("wss://relay.mememaps.net")
+await relay.connect()
+let answer = await relay.publish(event)
+
+console.log("answer")
+console.log(answer)
---
+++ my-schema/experiments/realTimeTest/sub.js
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
+import assert from "assert"
+import { generateSecretKey, getPublicKey, finalizeEvent, verifyEvent } from 'nostr-tools/pure'
+
+// const wsPublish = new WebSocket("ws://localhost:9090")
+// let kind_stats = {}
+
+// const wsListen = new WebSocket("https://archive.mememaps.net/")
+const wsListen = new WebSocket("http://localhost:9091")
+
+
+wsListen.addEventListener('open', async function (event) {
+    wsListen.send(JSON.stringify([
+        "REQ",
+        String(uuidv4()),
+        { kinds: [999]},
+    ]));
+});
+wsListen.addEventListener('message', function (event) {
+    console.log(`wsListen Message ${event.data}`)
+    console.log(JSON.stringify(JSON.parse(event.data), null, 2))
+})
